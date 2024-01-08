a647e1bb2c67ca5fdbae2afc254546e609c8d5d6
kshan1224 <41182890+kshan1224@users.noreply.github.com>
2024-01-08 14:10:46 +0900

    Add files via upload

--- Gemini.py
+++ Gemini.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 from PIL import Image
 import os 
 import io 
+#from streamlit_extras.buy_me_a_coffee import button
 
-load_dotenv()
+#button(username="rwar", floating=True, width=221)
+load_dotenv() 
 
 def image_to_byte_array(image: Image) -> bytes:
     imgByteArr = io.BytesIO()
@@ -20,10 +22,30 @@ API_KEY = os.environ.get("GOOGLE_API_KEY")
 genai.configure(api_key=API_KEY)
 
 
-st.image("./raoncute_logo.png", width=200)
-st.write("")
+# Define the custom CSS
+custom_css = """
+<style>
+    .stTextInput>div>div>input {
+        border: 2px solid #4CAF50; /* You can change the color and size of the border here */
+    }
+    .big-font {
+        font-size: 10px; /* Change the size as needed */
+    }
+</style>
+"""
 
-gemini_pro, gemini_vision = st.tabs(["Gemini Pro", "Gemini Pro Vision"])
+# Inject the custom CSS with st.markdown
+st.markdown(custom_css, unsafe_allow_html=True)
+#st.header("무엇이든 물어보세요.")
+# Create two columns
+col1, col2 = st.columns([1, 4])
+
+# Display the image in the first column
+col1.image("./ai_image.jpg", width=100)
+# Display the text in the second column
+col2.header("라온 챗봇에 오신걸 환영합니다.")
+
+gemini_vision, gemini_pro  = st.tabs(["AI 로또번호생성", "대화형 챗봇(텍스트)"])
 # HTML 코드 삽입
 html_code = """
 <ins class="kakao_ad_area" style="display:none;"
@@ -33,33 +55,13 @@ data-ad-height="100"></ins>
 <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
 """
 
+
 def main():
-    with gemini_pro:
-        st.header("무엇이든 물어보세요.")
-
-        
-        
-        prompt = st.text_input("prompt please...", placeholder="Prompt", label_visibility="visible")
-        model = genai.GenerativeModel("gemini-pro")
-
-        if st.button("SEND",use_container_width=True):
-            response = model.generate_content(prompt)
-
-            st.write("")
-            st.header(":blue[Response]")
-            st.write("")
-
-            st.markdown(response.text)
-            
-
-
     with gemini_vision:
-        st.header("이미지를 올리고 무엇이든 물어보세요.\n 예를 들면 얼굴사진을 올리고 '사진과 관련된 사람의 운세를 알려주세요.'를 입력하시면 됩니다.")
-        st.write("")
 
-        image_prompt = st.text_input("Interact with the Image", placeholder="Prompt", label_visibility="visible")
-        uploaded_file = st.file_uploader("Choose and Image", accept_multiple_files=False, type=["png", "jpg", "jpeg", "img", "webp"])
-
+        #image_prompt = st.text_input("Interact with the Image", placeholder="Prompt", label_visibility="visible")
+        uploaded_file = st.file_uploader("나와 연관된 이미지를 올리고 로또번호생성 버튼을 눌러주세요.", accept_multiple_files=False, type=["png", "jpg", "jpeg", "img", "webp"])
+        image_prompt ="Generate 6 random numbers between 1 and 45 for the Korean Lotto."
         if uploaded_file is not None:
             st.image(Image.open(uploaded_file), use_column_width=True)
 
@@ -71,7 +73,7 @@ def main():
                 </style>
                 """, unsafe_allow_html=True)
             
-        if st.button("GET RESPONSE", use_container_width=True):
+        if st.button("로또번호생성", use_container_width=True):
             model = genai.GenerativeModel("gemini-pro-vision")
 
             if uploaded_file is not None:
@@ -95,10 +97,11 @@ def main():
                     response.resolve()
 
                     st.write("")
-                    st.write(":blue[Response]")
+                    st.write(":blue[답변]")
                     st.write("")
 
                     st.markdown(response.text)
+                    #st.markdown(f'<div class="big-font">{response.text}</div>', unsafe_allow_html=True)
 
                 else:
                     st.write("")
@@ -107,8 +110,27 @@ def main():
             else:
                 st.write("")
                 st.header(":red[Please Provide an image]")
+    with gemini_pro:
+
+        prompt = st.text_input("아래 입력란에 궁금한 내용을 적어주세요.", placeholder="예시) AI 기술은 한국인의 일상생활에 어떤 방식으로 접목되고 있나요?", label_visibility="visible")
+        model = genai.GenerativeModel("gemini-pro")
+
+        if st.button("질문 보내기",use_container_width=True):
+            response = model.generate_content(prompt)
+
+            st.write("")
+            st.header(":blue[답변]")
+            st.write("")
+
+            st.markdown(response.text)
 #st.markdown(html_code, unsafe_allow_html=True)
 st.components.v1.html(html_code, height=150, scrolling=True)
+# iframe을 사용하여 HTML 코드를 삽입
+# Read the AdSense HTML code from the file
+with open("index.html", "r") as file:
+    ad_code_html = file.read()
+
+
 if __name__ == "__main__":
     main()
 
